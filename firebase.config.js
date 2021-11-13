2816a1e08680d1ef11945716622b44a9d1ba61a8
KOCSIS RÓBERT ZOLTÁN <robertzkocsis@gmail.com>
2021-11-13 10:00:29 +0100

    Add firebase config with new project (fixed typo)

--- firebase.config.js
+++ firebase.config.js
@@ -7,13 +7,13 @@ import { getAnalytics } from 'firebase/analytics';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: 'AIzaSyCy7zxincFZNYzXolRmCqFEemZVdsLeCOY',
-  authDomain: 'cord-change-trainer.firebaseapp.com',
-  projectId: 'cord-change-trainer',
-  storageBucket: 'cord-change-trainer.appspot.com',
-  messagingSenderId: '660012955986',
-  appId: '1:660012955986:web:8cd6fc7454cc99995ea28b',
-  measurementId: 'G-89Q66P7E8N',
+  apiKey: 'AIzaSyAELgoVepkpU8iKMMa7R5-T60hssGf3BIs',
+  authDomain: 'chord-change-trainer.firebaseapp.com',
+  projectId: 'chord-change-trainer',
+  storageBucket: 'chord-change-trainer.appspot.com',
+  messagingSenderId: '824300373293',
+  appId: '1:824300373293:web:b84a7f9a1e953cdc38bfa4',
+  measurementId: 'G-5C7TS2KFF4',
 };
 
 // Initialize Firebase
